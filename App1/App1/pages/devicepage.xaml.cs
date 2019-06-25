659455090e2732d882495fa9eb2b272b4237e125
sidneydijkstra <sidney0099@hotmail.com>
2019-06-25 14:11:42 +0200

    first commit

---
+++ App1/App1/pages/devicepage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.pages{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class devicepage : ContentPage{
+
+        private Device _device;
+        private ObservableCollection<Command> _commands = new ObservableCollection<Command>();
+
+        public devicepage(Device _device){
+			InitializeComponent();
+            this._device = _device;
+            CommandList.ItemsSource = this._commands;
+            foreach (Command command in _device.commands){
+                _commands.Add(command);
+            }
+            initPage();
+
+        }
+
+        private void initPage() {
+            deviceName.Text = "Device: " + _device.id;
+            deviceDesc.Text = _device.description;
+        }
+	}
+}
